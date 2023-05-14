59ed0221a874cf005cf488db32d1438513d797af
danakal <dana.kalcikova@centrum.cz>
2023-05-14 16:49:33 +0200

    generics

--- src/generics.ts
+++ src/generics.ts
@@ -63,3 +63,25 @@ textStorage.addItem('Idaho');
 console.log(textStorage.getItems());
 textStorage.removeItem('Dana');
 console.log(textStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string,
+    description: string,
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+//names.push('Peter');
+//names.pop();
